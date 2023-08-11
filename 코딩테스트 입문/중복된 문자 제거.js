c82905dcc0ba75d3c05e265e9c3674a8fced7b29
jaejae990921 <jaejae990921@naver.com>
2023-08-11 13:28:03 +0900

    Lv. 0 | 중복된 문자 제거 | 2023.08.11

---
+++ 코딩테스트 입문/중복된 문자 제거.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+    let answer = "";
+    
+    for (let i = 0; i < my_string.length; i++) {
+        if (answer.includes(my_string[i])) {
+            continue;
+        } else {
+            answer += my_string[i];
+        }
+    }
+    
+    return answer;
+}
