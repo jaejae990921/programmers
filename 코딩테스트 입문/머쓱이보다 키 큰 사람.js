7676195b039d8168348175e738cb46b63996cbd0
jaejae990921 <jaejae990921@naver.com>
2023-08-17 16:14:32 +0900

    Lv. 0 | 머쓱이보다 키 큰 사람 | 2023.08.17

---
+++ 코딩테스트 입문/머쓱이보다 키 큰 사람.js
@@ -0,0 +1,11 @@
+function solution(array, height) {
+    let cnt = 0;
+    
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > height) {
+            cnt++;
+        }
+    }
+    
+    return cnt;
+}
