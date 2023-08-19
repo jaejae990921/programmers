99b779910c017cf3e666f2672bbc4440466accf0
jaejae990921 <jaejae990921@naver.com>
2023-08-19 23:40:35 +0900

    Lv. 0 | 안전지대 | 2023.08.19

---
+++ 코딩테스트 입문/안전지대.js
@@ -0,0 +1,34 @@
+function solution(board) {
+    let dx = [1, 0, -1, 0, 1, 1, -1, -1];
+    let dy = [0, 1, 0, -1, 1, -1, 1, -1];
+    let n = board.length;
+  
+    let answer = n * n;
+  
+    let ch = [];
+  
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n; j++) {
+        if (board[i][j] === 1) {
+          ch.push([i, j]);
+          answer--;
+        }
+      }
+    }
+  
+    if (answer === 0) return 0;
+  
+    ch.forEach((w) => {
+      for (let k = 0; k < 8; k++) {
+        const nx = w[0] + dx[k];
+        const ny = w[1] + dy[k];
+  
+        if (nx >= 0 && ny >= 0 && nx < n && ny < n && board[nx][ny] === 0) {
+          board[nx][ny] = 1;
+          answer--;
+        }
+      }
+    });
+  
+    return answer;
+  }
