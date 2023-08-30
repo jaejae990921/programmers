5bc6145c27ed830d3fb52fe1468e02bd2b2bd449
jaejae990921 <jaejae990921@naver.com>
2023-08-30 20:31:34 +0900

    Lv. 2 | 최솟값 만들기 | 2023.08.30

---
+++ 연습문제/최솟값 만들기.js
@@ -0,0 +1,11 @@
+function solution(A,B){
+    let a = A.sort((a,b) => a-b);
+    let b = B.sort((a,b) => b-a);
+    let sum = 0;
+    
+    for (let i = 0; i < a.length; i++) {
+        sum += (a[i] * b[i])
+    }
+    
+    return sum;
+}
