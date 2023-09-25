44ef3730461550f00e293d1d1f3ff6c580fa7168
jaejae990921 <jaejae990921@naver.com>
2023-09-25 23:07:38 +0900

    Lv. 0 | 배열의 원소만큼 추가하기 | 2023.09.25

---
+++ 코딩 기초 트레이닝/배열의 원소만큼 추가하기.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    let answer = [];
+    
+    arr.map((num) => {
+        for(let i = 0; i < num; i++) {
+            answer.push(num);
+        }
+    });
+    
+    return answer;
+}
