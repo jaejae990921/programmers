5806dad0d019612223d673b93f01d76af48da26b
jaejae990921 <jaejae990921@naver.com>
2023-09-25 23:08:02 +0900

    Lv. 0 | 길이에 따른 연산 | 2023.09.25

---
+++ 코딩 기초 트레이닝/길이에 따른 연산.js
@@ -0,0 +1,17 @@
+function solution(num_list) {
+    let answer;
+    
+    if (num_list.length >= 11) {
+        answer = 0;
+        for (let i = 0; i < num_list.length; i++) {
+            answer += num_list[i]
+        }
+    } else {
+        answer = 1;
+        for (let i = 0; i < num_list.length; i++) {
+            answer *= num_list[i]
+        }
+    }
+    
+    return answer;
+}
