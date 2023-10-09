d2fee1160726f68e960c9f8ecea01d8d1a9b3b86
jaejae990921 <jaejae990921@naver.com>
2023-10-09 17:50:53 +0900

    Lv. 1 | 폰켓몬 | 2023.10.09

---
+++ 해시/폰켓몬.js
@@ -0,0 +1,12 @@
+function solution(nums) {
+    let answer = [];
+    let max = nums.length / 2;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (!answer.includes(nums[i])) {
+            answer.push(nums[i]);
+        }
+    }
+    
+    return answer.length >= max ? max : answer.length;
+}
