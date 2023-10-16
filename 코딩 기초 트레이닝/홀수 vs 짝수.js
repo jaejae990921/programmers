556866f01840b636111fc43e903271d52fe7d5cb
jaejae990921 <jaejae990921@naver.com>
2023-10-16 14:38:00 +0900

    Lv. 0 | 홀수 vs 짝수 | 2023.10.16

---
+++ 코딩 기초 트레이닝/홀수 vs 짝수.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    let odd = 0;
+    let even = 0;
+    
+    for (let i = 0; i < num_list.length; i++) {
+        i % 2 == 0 ? even+=num_list[i] : odd+=num_list[i];
+    }
+    
+    return Math.max(odd, even);
+}
