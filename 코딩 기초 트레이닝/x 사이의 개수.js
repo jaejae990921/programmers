6dd7f530334d42053bc9b790b596bf36966a42cb
jaejae990921 <jaejae990921@naver.com>
2023-11-01 17:07:56 +0900

    Lv. 0 | x 사이의 개수 | 2023.11.01

---
+++ 코딩 기초 트레이닝/x 사이의 개수.js
@@ -0,0 +1,10 @@
+function solution(myString) {
+    let arr = myString.split("x");
+    let answer = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        answer.push(arr[i].length);
+    }
+    
+    return answer;
+}
