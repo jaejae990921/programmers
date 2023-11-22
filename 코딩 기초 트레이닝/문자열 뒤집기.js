a78e4c80a905f6dc57005a82b3d50a0a776bc007
jaejae990921 <jaejae990921@naver.com>
2023-11-22 23:12:41 +0900

    Lv. 0 | 문자열 뒤집기 | 2023.11.22

---
+++ 코딩 기초 트레이닝/문자열 뒤집기.js
@@ -0,0 +1,13 @@
+function solution(my_string, s, e) {
+    let answer = '';
+    
+    answer += my_string.slice(0, s);
+    
+    for (let i = e; i >= s; i--) {
+        answer += my_string.charAt(i);
+    }
+    
+    answer += my_string.slice(e+1);
+    
+    return answer;
+}
