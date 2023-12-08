c82cf9d2638580ce044c3fe09be88d9988ba510b
jaejae990921 <jaejae990921@naver.com>
2023-12-08 23:47:33 +0900

    Lv. 0 | 특별한 이차원 배열 1 | 2023.12.08

---
+++ 코딩 기초 트레이닝/특별한 이차원 배열 1.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let answer = [];
+    
+    for (let i = 0; i < n; i++) {
+        let arr = [];
+        for (let j = 0; j < n; j++) {
+            i == j ? arr.push(1) : arr.push(0);
+        }
+        answer.push(arr);
+    }
+    
+    return answer;
+}
